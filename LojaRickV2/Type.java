b066ac6e8c7427207f6a2b60802a65719f92e75e
heryanq <43239894+heryanq@users.noreply.github.com>
2019-04-27 13:49:56 -0300

    LojaRickV2

---
+++ LojaRickV2/Type.java
@@ -0,0 +1,14 @@
+package lojarickv2;
+
+public enum Type {
+	ACOUSTIC, ELECTRIC;
+
+	  public String toString() {
+	    switch(this) {
+	      case ACOUSTIC: return "acoustic";
+	      case ELECTRIC: return "electric";
+	      default:       return "unspecified";
+	    }
+	  }
+
+}
